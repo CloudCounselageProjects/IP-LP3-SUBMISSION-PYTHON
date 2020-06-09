bf8867d81747897235622f5121e14fe90af73c63
lanjewarrushikesh <66350113+lanjewarrushikesh@users.noreply.github.com>
2020-06-09 11:31:08 +0530

    Add files via upload

---
+++ question 1.py
@@ -0,0 +1,11 @@
+print ("Enter the value for a")
+a = int(input())
+print ("Enter the value for b")
+b = int(input())
+if 1<=a<=10**10 and 1<=b<=10**10:
+    sum = a+b
+    difference = a-b
+    product = a*b
+    print  ("Sum:",sum)
+    print ("Difference",difference)
+    print("Multiplication",product)
