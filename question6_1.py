b5002eef841c0db66c18e618cb1b4d87012e0207
rg226965 <35304793+rg226965@users.noreply.github.com>
2020-06-14 22:18:45 +0530

    Delete question6_1.py

--- question6_1.py
+++
@@ -1,9 +0,0 @@
-string = input()
-if 'not' in string and 'poor' in string:
-    if string.find('not') < string.find('poor'):
-        string = string[0:string.find('not')] + 'good' + string[string.find('poor') + 4:]
-        print(string)
-    else:
-        print(string)
-else:
-    print(string)
