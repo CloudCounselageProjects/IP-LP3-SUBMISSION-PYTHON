c1a722ddf7c360b1894281b5ee9af2794d3c301b
Anirudh Belwadi <42411311+anirudhbelwadi@users.noreply.github.com>
2020-06-14 22:32:22 +0530

    Delete leap_year.py

--- leap_year.py
+++
@@ -1,9 +0,0 @@
-def leap(a):
-        if 1900<=a<=10**10:
-                if(a%4==0 and a%400==0 and a&100!=0):
-                        return True
-                else:
-                        return False
-year=int(input())
-x=leap(year)
-print(x)
